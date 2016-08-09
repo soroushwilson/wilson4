4dfc2164596962ee218973edbdf530f52c609456
alfred regular <alfred-regular@users.noreply.github.com>
2016-08-10 04:20:07 +0430

    Update Stats.lua

--- plugins/Stats.lua
+++ plugins/Stats.lua
@@ -94,7 +94,7 @@ local function run(msg, matches)
   if matches[1]:lower() == 'signal' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /signal ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /krasus ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
