0b1858700b267e2a6a2bede4f69df51eef5a2e95
alfred regular <alfred-regular@users.noreply.github.com>
2016-08-10 04:11:53 +0430

    Update and rename InvPouria.lua to Addsaman.lua

--- plugins/Addsaman.lua
+++ plugins/Addsaman.lua
@@ -6,10 +6,10 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 175636120
+  local user = 221633324
 
-  if matches[1] == "invpouria" then
-    user = 'user#id'..175636120
+  if matches[1] == "Addsaman" then
+    user = 'user#id'..221633324
   end
 
   -- The message must come from a chat group
@@ -24,12 +24,12 @@ local function run(msg, matches)
 end
 
 return {
-  description = "invsudo", 
+  description = "addsudo", 
   usage = {
     "/invite name [user_name]", 
     "/invite id [user_id]" },
   patterns = {
-    "^[!/#](invpouria)$"
+    "^[!/#](addsaman)$"
   }, 
   run = run 
 }
