c259115b850edce1cc5b4ffd807ace57400512b6
alfred regular <alfred-regular@users.noreply.github.com>
2016-08-10 08:50:39 +0430

    Update Supergroup.lua

--- plugins/Supergroup.lua
+++ plugins/Supergroup.lua
@@ -102,18 +102,6 @@ else
     send_large_msg(cb_extra.receiver, text)
 end
 
---Get and output info about supergroup
-local function callback_info(cb_extra, success, result)
-local title ="Info for SuperGroup: ["..result.title.."]\n\n"
-local admin_num = "Admin count: "..result.admins_count.."\n"
-local user_num = "User count: "..result.participants_count.."\n"
-local kicked_num = "Kicked user count: "..result.kicked_count.."\n"
-local channel_id = "ID: "..result.peer_id.."\n"
-if result.username then
-	channel_username = "Username: @"..result.username
-else
-	channel_username = ""
-end
 local text = title..admin_num..user_num..kicked_num..channel_id..channel_username
     send_large_msg(cb_extra.receiver, text)
 end
