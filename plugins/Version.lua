2f1c4e2319dc97588e0acdbd8cbdf4b67941b340
alfred regular <alfred-regular@users.noreply.github.com>
2016-08-10 05:27:30 +0430

    Add files via upload

---
+++ plugins/Version.lua
@@ -0,0 +1,28 @@
+do
+
+function run(msg, matches)
+  return 'krasus Bot v0.1'.. [[ 
+  
+  Channel: @krasus_team
+  dev & found: @alfred_regular
+  sudo & manager: @vvirus
+  
+Powered by:
+  alfred & dani & amour & mac
+  website:
+
+coming soon☺ 
+
+http://telegram.me/krasustg]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/#]ver$"
+  }, 
+  run = run 
+}
+
+end
