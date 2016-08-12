6fa42f74c8250063f6a7983bdc686c4dab3c4283
alfred regular <alfred-regular@users.noreply.github.com>
2016-08-12 13:26:15 +0430

    Update Remove_Plugin.lua

--- plugins/Remove_Plugin.lua
+++ plugins/Remove_Plugin.lua
@@ -1,11 +1,15 @@
-function run(msg, matches)
-text = io.popen("cd plugins && rm -rf  " .. matches[1]):read('*all')
-  return text
+
+local function run(msg, matches)
+    if matches[1] == "delplugin" and is_sudo(msg) then
+text = io.popen("cd plugins && rm "..matches[2]..".lua")
+return text.."پلاگین ["..matches[2].."] با موفقیت حذف شد"
+end 
 end
-return {
-  patterns = {
-    "^[!/#]remplug (.*)$"
-  },
-  run = run,
-  moderated = true
+
+return { 
+patterns = {
+ 
+'^[!/#](delplugin) (.*)$' 
+},
+run = run,
 }
