e2fed386a841bbe539309b5763ddf877852e4e06
alfred regular <alfred-regular@users.noreply.github.com>
2016-08-12 17:26:21 +0430

    Create block.lua

---
+++ plugins/block.lua
@@ -0,0 +1,33 @@
+do
+
+local function block_user_callback(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  local user = 'user#id'..result.id
+  if success == 0 then
+    return send_large_msg(receiver, "i'cant blocked this user im sorry dady")
+  end
+  block_user(user, cb_ok, false)
+end
+end
+local function run(msg, matches)
+ if msg.to.type == 'chat' then
+    local user = 'chat#id'..msg.to.id
+ local user = matches[2]
+  if matches[1] == "user" then
+      user = 'user#id'..user
+      block_user(user, callback, false)
+    end
+    if not is_sudo(msg) then
+    return "just allowed for babam"
+  end
+    return "user is blocked"
+  end
+end
+ 
+return {
+  patterns = {
+    "^#block (user) (%d+)$",
+  },
+  run = run,
+
+}
